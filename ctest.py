edf484e80c270e792182ec37ddf990f2ce095478
Piyush Mishra <141337183+coderbie@users.noreply.github.com>
2024-09-08 15:49:16 +0530

    Add files via upload

---
+++ ctest.py
@@ -0,0 +1,76 @@
+import asyncio
+import aiofiles
+import os
+
+async def request_file(reader, writer):
+    filename = input("Requesting filename: ")
+    writer.write(filename.encode())
+    await writer.drain()
+
+    is_file = await reader.read(255)
+
+    if is_file == b'file_available':
+        async with aiofiles.open(f"recieved_{filename}", 'wb') as f:
+            while True:
+                data = await recv_chunk(reader)
+                # print(f"{data}\n")
+                if data == b'eof':
+                    break
+                await f.write(data)
+        print("Sucessfully recieved file.")
+    else:
+        print("File is not on server.")
+
+async def upload_file(filename, reader, writer):
+    writer.write(filename.encode())
+    await writer.drain()
+    is_duplicate = await reader.read(255)
+    if is_duplicate == b'no_duplicate_file':
+        async with aiofiles.open(filename, 'rb') as f:
+            while True:
+                data = await f.read(4096)
+                if not data:
+                    await send_chunk(b'eof', writer)
+                    break
+                await send_chunk(data, writer)
+        print("File was uploaded on server sucessufully.")
+    else:
+        print("File with same name is already on server.")
+
+async def recv_chunk(reader):
+    prefix = await reader.readline()
+    chunk_len = int(prefix)
+    return await reader.readexactly(chunk_len)
+
+async def send_chunk(content, writer):
+    writer.write(b'%d\n' % len(content))
+    writer.write(content)
+    await writer.drain()
+
+async def main():
+    try:
+        reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+        while True:
+            command = input("command: ")
+            if command == "0":
+                writer.write('0'.encode())
+                await writer.drain()
+                writer.close()
+                await writer.wait_closed()
+                print("Disconnected from server.")
+                break
+            if command == "1":
+                writer.write('1'.encode())
+                await writer.drain()
+                await request_file(reader, writer)
+            if command == "2":
+                filename = input("Uploading filename: ")
+                if os.path.isfile(filename):
+                    writer.write('2'.encode())
+                    await writer.drain()
+                    await upload_file(filename, reader, writer)
+    except ConnectionResetError:
+        print("✻ Connection lost from the server or server is closed.")
+    except ConnectionRefusedError:
+        print("✻ Connection denied or server is not available.")
+asyncio.run(main())
---
+++ test.py
@@ -0,0 +1,84 @@
+import asyncio
+import os
+import aiofiles
+
+async def handle_client(reader, writer):
+    try:
+        addr = writer.get_extra_info('peername')
+        while True:
+            print(f"waiting for request from {addr}")
+            request = await reader.read(1)
+            request = request.decode()
+            print(f"command {request} from {addr}")
+            if request == "0":
+                print(f'Closing connection of {addr}')
+                writer.close()
+                await writer.wait_closed()
+                print(f"✻ Client {addr} disconnected.")
+                break
+            if request == "1":
+                await send_file(reader, writer)
+            if request == "2":
+                await download_file(reader, writer)
+    except ConnectionResetError:
+        print(f"✻ {addr} The specified network is no longer available.")
+
+async def send_file(reader, writer):
+    filename = (await reader.read(1024)).decode()
+    
+    if os.path.isfile(filename):
+        writer.write(b'file_available')
+        await writer.drain()
+
+        async with aiofiles.open(filename, 'rb') as f:
+            while True:
+                data = await f.read(4096)
+                if not data:
+                    await send_chunk(b'eof', writer)
+                    break
+                await send_chunk(data, writer)
+        print("File was sent sucessfully.")
+    else:
+        writer.write(b'file_not_available')
+        await writer.drain()
+        print("Requested file is not on server.")
+
+async def download_file(reader, writer):
+    filename = (await reader.read(1024)).decode()
+    if not os.path.isfile(f"uploaded_{filename}"):
+        writer.write(b'no_duplicate_file')
+        await writer.drain()
+        async with aiofiles.open(f"uploaded_{filename}", 'wb') as f:
+            while True:
+                data = await recv_chunk(reader)
+                if data == b'eof':
+                    break
+                await f.write(data)
+        print("Sucessfully downloaded file.")
+    else:
+        writer.write(b'duplicate_file')
+        await writer.drain()
+        print("File with same name is on server.")
+
+async def recv_chunk(reader):
+    prefix = await reader.readline()
+    chunk_len = int(prefix)
+    return await reader.readexactly(chunk_len)
+         
+async def send_chunk(content, writer):
+    writer.write(b'%d\n' % len(content))
+    writer.write(content)
+    await writer.drain()           
+
+async def main():
+    server = await asyncio.start_server(handle_client, '127.0.0.1', 8888)
+
+    async with server:
+        await server.serve_forever()
+    
+
+asyncio.run(main())
+
+# print('1'.encode())
+# print(os.path.getsize("i.png"))
+# print(int(5))
